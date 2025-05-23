4bac133f27db201d9821076f1a3ea2dd0289f376
Hikari <zhanghanyu2001@outlook.com>
2025-05-24 03:36:37 +0800

    this is a test commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Hello
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello Ubuntu");
+	}
+}
